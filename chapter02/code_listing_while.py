bd21e4212db3a6332749c065989188400e19c647
Joe Paris <joe.paris@outlook.com>
2020-01-22 09:24:45 -0800

    Add loop parts labels

--- chapter02/code_listing_while.py
+++ chapter02/code_listing_while.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python3.7
 import time
 
-count = 10
+count = 10  # loop control varialbe
 
-while count >= 0:
+while count >= 0:  # test loop condition
     print(f"{count}...")
     time.sleep(0.5)
-    count -= 1
+    count -= 1  # modify the loop control variable
 
 print("🚀  Blast off!! 🚀")
