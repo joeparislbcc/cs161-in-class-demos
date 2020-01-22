14c68fed280ae954b400eddb8c3d05cfbf52f5b1
Joe Paris <joe.paris@outlook.com>
2020-01-22 09:24:20 -0800

    Add shebang

--- chapter02/code_listing_2.28.py
+++ chapter02/code_listing_2.28.py
@@ -1,20 +1,21 @@
+#! /usr/bin/env python3.7
 # Copyright 2017, 2013, 2011 Pearson Education, Inc., W.F. Punch & R.J.Enbody
 # plot a sine wave from 0 to 4pi
 
 import math
 import pylab
 
-#initialize the two lists and the counting variable num. Note is a float
+# initialize the two lists and the counting variable num. Note is a float
 y_values = []
 x_values = []
 number = 0.0
 
-#collect both number and the sine of number in a list
+# collect both number and the sine of number in a list
 while number < math.pi * 4:
     y_values.append(math.sin(number))
     x_values.append(number)
     number += 0.1
 
-#plot the x and y values as red circles
-pylab.plot(x_values,y_values,'ro')
+# plot the x and y values as red circles
+pylab.plot(x_values, y_values, "ro")
 pylab.show()
