8050784d37d1b8bae48d383198ec82d8cfacadf8
Joe Paris <joe.paris@outlook.com>
2020-01-21 16:58:29 -0800

    Add listing 1.4

---
+++ chapter01/0800/listing_1.4.py
@@ -0,0 +1,23 @@
+"""Convert Code Listing 1.4 to work outside of the Python REPL."""
+
+import turtle
+
+win = turtle.Screen()
+win.setup(400, 400)
+
+tommy = turtle.Turtle()
+tommy.speed(1)
+
+tommy.forward(100)
+tommy.right(144)
+tommy.forward(100)
+tommy.right(144)
+tommy.forward(100)
+tommy.right(144)
+tommy.forward(100)
+tommy.right(144)
+tommy.forward(100)
+
+tommy.hideturtle()
+
+win.mainloop()
